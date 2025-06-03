3b28f7468ba7e5a70a1404a9a8792683534127c9
mslmn <55067911+mslmn@users.noreply.github.com>
2025-06-03 01:11:10 +0100

    completed challenge 2

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,60 @@
-const db = require("../connection")
+const db = require("../connection");
 
 const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics;`);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE TOPICS (
+          slug VARCHAR PRIMARY KEY,
+          description VARCHAR NOT NULL,
+          img_url VARCHAR(1000) NOT NULL
+          );
+          `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE users (
+          username VARCHAR PRIMARY KEY,
+          name VARCHAR NOT NULL,
+          avatar_url VARCHAR(1000) NOT NULL
+          );
+        `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE articles (
+          article_id SERIAL PRIMARY KEY,
+          title VARCHAR NOT NULL,
+          topic VARCHAR REFERENCES topics(slug) ON DELETE CASCADE,
+          author VARCHAR REFERENCES users(username) ON DELETE CASCADE,
+          body TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          votes INT DEFAULT 0,
+          article_img_url VARCHAR(1000) NOT NULL
+         );
+        `);
+    })
+    .then(() => {
+      return db.query(`
+        CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          article_id INT REFERENCES articles(article_id) ON DELETE CASCADE,
+          body TEXT NOT NULL,
+          votes INT DEFAULT 0,
+          author VARCHAR REFERENCES users(username) ON DELETE CASCADE,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+         );
+        `);
+    });
 };
 module.exports = seed;
