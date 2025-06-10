82b31ffa56f9358dcd3d2ad2ba0d481f8c1c1cbb
mslmn <55067911+mslmn@users.noreply.github.com>
2025-06-10 14:05:39 +0100

    fix connection.js

--- db/connection.js
+++ db/connection.js
@@ -2,14 +2,6 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
 
-require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
-
-const db = new Pool();
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
 const config = {};
 
 if (ENV === "production") {
@@ -17,6 +9,14 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-module.exports = new Pool(config);
+require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
+} else {
+  console.log(`Connected to ${process.env.PGDATABASE || "production database"}`);
+}
+
+const db = new Pool(config);
 
 module.exports = db;
